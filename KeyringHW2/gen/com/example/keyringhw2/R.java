3900b5a13c824b1a25b5744a4699dfa9d8536343
Earthterror <adrianbsandu@gmail.com>
2013-11-11 13:34:55 +0200

    Back2Hell

--- KeyringHW2/gen/com/example/keyringhw2/R.java
+++ KeyringHW2/gen/com/example/keyringhw2/R.java
@@ -25,45 +25,63 @@ public final class R {
         public static final int ic_action_new=0x7f020001;
         public static final int ic_action_remove=0x7f020002;
         public static final int ic_launcher=0x7f020003;
+        public static final int ic_launcher_boss=0x7f020004;
+        public static final int ic_launcher_second_icon=0x7f020005;
     }
     public static final class id {
-        public static final int action_edit=0x7f080005;
-        public static final int action_new=0x7f080004;
-        public static final int action_remove=0x7f080006;
-        public static final int action_settings=0x7f080003;
-        public static final int button1=0x7f080000;
-        public static final int editText1=0x7f080002;
-        public static final int textView1=0x7f080001;
+        public static final int action_edit=0x7f08000a;
+        public static final int action_new=0x7f080009;
+        public static final int action_remove=0x7f08000b;
+        public static final int action_settings=0x7f080008;
+        public static final int btn_cancel=0x7f080007;
+        public static final int btn_launch=0x7f080001;
+        public static final int btn_login=0x7f080006;
+        public static final int button1=0x7f080002;
+        public static final int editText1=0x7f080003;
+        public static final int password=0x7f080005;
+        public static final int textView1=0x7f080000;
+        public static final int txt_name=0x7f080004;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_second=0x7f030001;
+        public static final int activity_android_dialog=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_second=0x7f030002;
+        public static final int dialog_layout=0x7f030003;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
-        public static final int main_activity_actions=0x7f070001;
-        public static final int second=0x7f070002;
+        public static final int android_dialog=0x7f070000;
+        public static final int list_view_loader=0x7f070001;
+        public static final int main=0x7f070002;
+        public static final int main_activity_actions=0x7f070003;
+        public static final int second=0x7f070004;
     }
     public static final class string {
         public static final int action_edit=0x7f050004;
-        public static final int action_forgot_password=0x7f05000e;
+        public static final int action_forgot_password=0x7f050015;
         public static final int action_new=0x7f050003;
         public static final int action_remove=0x7f050005;
         public static final int action_settings=0x7f050001;
-        public static final int action_sign_in_register=0x7f05000c;
-        public static final int action_sign_in_short=0x7f05000d;
+        public static final int action_sign_in_register=0x7f050013;
+        public static final int action_sign_in_short=0x7f050014;
         public static final int app_name=0x7f050000;
-        public static final int error_field_required=0x7f050013;
-        public static final int error_incorrect_password=0x7f050012;
-        public static final int error_invalid_email=0x7f050010;
-        public static final int error_invalid_password=0x7f050011;
+        public static final int btn_launch=0x7f05000c;
+        public static final int dialog_cancel=0x7f05000b;
+        public static final int dialog_submit=0x7f05000a;
+        public static final int dialog_uname=0x7f050009;
+        public static final int error_field_required=0x7f05001a;
+        public static final int error_incorrect_password=0x7f050019;
+        public static final int error_invalid_email=0x7f050017;
+        public static final int error_invalid_password=0x7f050018;
         public static final int hello_world=0x7f050002;
-        public static final int login_progress_signing_in=0x7f05000f;
+        public static final int keyring_apllication=0x7f05000e;
+        public static final int login_progress_signing_in=0x7f050016;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f05000a;
-        public static final int prompt_password=0x7f05000b;
-        public static final int title_activity_login=0x7f050009;
+        public static final int prompt_email=0x7f050011;
+        public static final int prompt_password=0x7f050012;
+        public static final int start_the_application=0x7f05000f;
+        public static final int textView1=0x7f05000d;
+        public static final int title_activity_login=0x7f050010;
         public static final int title_activity_sec=0x7f050006;
         public static final int title_activity_second=0x7f050007;
         public static final int welcome=0x7f050008;
--- KeyringHW2/src/com/example/keyringhw2/MainActivity.java
+++ KeyringHW2/src/com/example/keyringhw2/MainActivity.java
@@ -4,8 +4,11 @@ package com.example.keyringhw2;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
@@ -17,11 +20,10 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
 	
 	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+	//@Override
+	//protected void onCreate(Bundle savedInstanceState) {
+		
+	//}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -31,7 +33,7 @@ public class MainActivity extends Activity {
 				
 		
 	}
-	
+	/**
 	public void methodOne(View v){
 		
 		EditText et = (EditText) findViewById(R.id.editText1);
@@ -50,12 +52,101 @@ public class MainActivity extends Activity {
              int duration = Toast.LENGTH_SHORT;
 
              Toast.makeText(context, "Wrong pass bitch!", duration).show();
-		}
-		
+		} */
+	
+	final private static int DIALOG_LOGIN = 1;
+
+	 @Override
+	 public void onCreate(Bundle savedInstanceState) {
+	  super.onCreate(savedInstanceState);
+	  setContentView(R.layout.activity_android_dialog);
+
+	  Button launch_button = (Button) findViewById(R.id.btn_launch);
+
+	  launch_button.setOnClickListener(new View.OnClickListener() {
+
+	  
+	@SuppressWarnings("deprecation")
+	@Override
+	   public void onClick(View v) {
+	    showDialog(DIALOG_LOGIN);
+	   }
+	  });
+	 }
+
+	 @Override
+	 protected Dialog onCreateDialog(int id) {
+
+	  AlertDialog dialogDetails = null;
+
+	  switch (id) {
+	  case DIALOG_LOGIN:
+	   LayoutInflater inflater = LayoutInflater.from(this);
+	   View dialogview = inflater.inflate(R.layout.dialog_layout, null);
+
+	   AlertDialog.Builder dialogbuilder = new AlertDialog.Builder(this);
+	   dialogbuilder.setTitle("Login");
+	   dialogbuilder.setView(dialogview);
+	   dialogDetails = dialogbuilder.create();
+
+	   break;
+	  }
+
+	  return dialogDetails;
+	 }
+
+	 @Override
+	 protected void onPrepareDialog(int id, Dialog dialog) {
+
+	  switch (id) {
+	  case DIALOG_LOGIN:
+	   final AlertDialog alertDialog = (AlertDialog) dialog;
+	   Button loginbutton = (Button) alertDialog
+	     .findViewById(R.id.btn_login);
+	   Button cancelbutton = (Button) alertDialog
+	     .findViewById(R.id.btn_cancel);
+	   final EditText userName = (EditText) alertDialog
+	     .findViewById(R.id.txt_name);
+	   final EditText password = (EditText) alertDialog
+	     .findViewById(R.id.password);
+
+	   loginbutton.setOnClickListener(new View.OnClickListener() {
+
+	    @Override
+	    public void onClick(View v) {
+	    	
+	    	if(password.getText().toString().equals("a"))
+			{
+			Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+	       startActivity(intent);
+			}
+			else
+			{
+				 Context context = MainActivity.this;
+	             
+	             int duration = Toast.LENGTH_SHORT;
+
+	             Toast.makeText(context, "Invalid password, please try again!", duration).show();
+	    	
+	     alertDialog.dismiss();
+	     
+	  }
+	    }});
+
+	   cancelbutton.setOnClickListener(new View.OnClickListener() {
+
+	    @Override
+	    public void onClick(View v) {
+	     alertDialog.dismiss();
+	    }
+	   });
+	   break;
+	  }
+	 }
 	}
 	
 	
 	
 	
 
-}
+
